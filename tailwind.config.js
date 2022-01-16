6f033fd5a75af8cac673b574628847db8ab74d2a
Beatriz Moura <beatrizmouradev@gmail.com>
2022-01-16 11:13:27 -0300

    :tada: Projeto finalizado

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
+//arquivo onde é aplicado a personalização (extend)
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        rosaClaro: "#B68D8D",
+        rosaEscuro: "#724646",
+        fundo: "#F0EBEB",
+        preto: "#373741"
+      },
+    },
   },
   plugins: [],
 }
